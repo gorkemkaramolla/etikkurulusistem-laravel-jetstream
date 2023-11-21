ae1f102faffb62f132ec2aec8755b0f662472321
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2023-11-21 12:51:50 +0300

    Gates defined

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,14 +14,15 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
-        //
+
+        $this->registerPolicies();
+
+        Gate::define('access-dashboard', function ($user) {
+            return $user->hasAnyRole(['sekreterlik', 'etik_kurul']);
+        });
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,13 @@
 <x-app-layout class="flex items-center justify-center">
+    <x-received-form :forms="$forms" />
 
-    <div class="w-full h-full bg-red-100">
-        <div class="flex w-full h-full justify-center items-center">
-            Dashboard
-        </div>
+    <div class="flex w-full h-full justify-center items-center">
+        @if (auth()->user()->hasRole('sekreterlik') ||
+                auth()->user()->hasRole('etik_kurul'))
+        @else
+            <p>Unauthorized access to the dashboard.</p>
+        @endif
     </div>
+    </div>
+
 </x-app-layout>
