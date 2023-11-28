5c2aad6f1dca3b3c97e1afd206e3b3524def0cc1
enguefto <unsalsvnc@gmail.com>
2023-11-28 23:06:05 +0300

    researcher-info name's added

--- resources/views/components/forms/researcher-informations.blade.php
+++ resources/views/components/forms/researcher-informations.blade.php
@@ -7,37 +7,37 @@
         <div class="col-md-4">
             <label for="researcherName" class="form-label">Araştırmacı Soyadı <br><span
                     class="pt-0 small">Lastname</span></label>
-            <input type="text" name="lastname" class="form-control" id="researcherName">
+            <input type="text" name="lastname" class="form-control">
         </div>
         <div class="col-md-4">
             <label for="studentID" class="form-label">Öğrenci No / TC No<br><span class="pt-0 small">Student ID
                     Number</span></label>
-            <input type="text" class="form-control" id="studentID" placeholder="">
+            <input type="text" name="student_no" class="form-control" id="studentID" placeholder="">
         </div>
         <div class="col-md-4">
             <label for="email" class="form-label">Mail Adressi<br><span class="pt-0 small">Email
                     Address</span></label>
-            <input type="email" class="form-control" id="email">
+            <input type="email" name="email" class="form-control" id="email">
         </div>
         <div class="col-md-4">
             <label for="phone" class="form-label">Telefon<br><span class="pt-0 small">Phone
                     Number</span></label>
-            <input type="phone" class="form-control" id="phone">
+            <input type="phone" name="gsm" class="form-control" id="phone">
         </div>
         <div class="col-md-4">
             <label for="advisor" class="form-label">Danışman / Yürütücü<br><span
                     class="pt-0 small">Advisor/Executive</span></label>
-            <input type="text" class="form-control " id="advisor">
+            <input type="text" name="advisor" class="form-control " id="advisor">
         </div>
         <div class="col-md-4">
             <label for="department" class="form-label">Anabilim Dalı<br><span
                     class="pt-0 small">Department</span></label>
-            <input type="text" class="form-control " id="department" placeholder="">
+            <input type="text" name="department" class="form-control " id="department" placeholder="">
         </div>
         <div class="col-md-4">
             <label for="program" class="form-label">Program<br>
                 <span class="pt-0 small">Department</span></label>
-            <input type="text" class="form-control" id="program">
+            <input type="text" name="major" class="form-control" id="program">
         </div>
     </div>
 </div>
